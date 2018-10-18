1ba866c8fe84b3f5f2bf9a63951e9998b1c7b2a6
kiril <kirilow@gmail.com>
2018-10-18 22:39:03 +0300

    fix footer

--- htmls/footer/footer.php
+++ htmls/footer/footer.php
@@ -9,16 +9,15 @@
 					<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
 				</div>
 			</div>
-        </footer>
-
-		<script type="text/javascript" src="/pub/js/jquery-3.2.1.min.js"></script>
-		<script type="text/javascript" src="/pub/js/materialize.min.js"></script>
-		<script type="text/javascript" src="/pub/js/main.js"></script>
-		<?php
-			foreach ($footer_js as $js) {
-				echo $js.PHP_EOL;
-			};
-		?>
+			<script type="text/javascript" src="/pub/js/jquery-3.2.1.min.js"></script>
+			<script type="text/javascript" src="/pub/js/materialize.min.js"></script>
+			<script type="text/javascript" src="/pub/js/main.js"></script>
+			<?php
+				foreach ($footer_js as $js) {
+					echo $js.PHP_EOL;
+				};
+			?>
+		</footer>
     </body>
 </html>
 
